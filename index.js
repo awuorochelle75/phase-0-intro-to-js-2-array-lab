f27e89cb381f3716770d7c508071f430afa1a4dc
Rochelle Awuor <awuorochelle75@gmail.com>
2025-02-27 15:52:04 +0300

    adds index.js

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution here!
+const cats=["Milo","Otis","Garfield"]
+
+function destructivelyAppendCat(Ralph){
+    cats.push("Ralph")
+}
+function destructivelyPrependCat(name){
+    cats.unshift(name)
+}
+function destructivelyRemoveLastCat(name) {
+    cats.pop(name)
+}
+function destructivelyRemoveFirstCat(name){
+    cats.shift(name)
+}
+function appendCat(name){
+    let newCats = cats.slice();
+  newCats.push(name);      
+  return newCats; 
+}
+function prependCat(name){
+
+        return [name].concat(cats.slice(0));
+      }
+function removeLastCat(){
+    return cats.slice(0, cats.length - 1)
+}
+function removeFirstCat(){
+    return cats.slice(1 )
+}
